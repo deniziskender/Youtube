e51cd9e8e2eedc5bfc24f7982f531d30be400f8c
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-03 17:16:50 +0300

    Siir

---
+++ Java/01/Siir.java
@@ -0,0 +1,13 @@
+public class Siir {
+
+	public static void main(String[] args) {
+
+		System.out.println("Artık demir almak günü gelmişse zamandan");
+		System.out.println("Meçhule giden bir gemi kalkar bu limandan.");
+		
+		System.out.println();
+		
+		System.out.println("Hiç yolcusu yokmuş gibi sessizce alır yol;");
+		System.out.println("Sallanmaz o kalkışta ne mendil, ne de bir kol.");
+	}
+}
