be92eeaa141ef80d5bfa2388a7fb5af4ff01d157
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-04 00:20:09 +0300

    Create VeriTipleri.java

---
+++ Java/02/VeriTipleri.java
@@ -0,0 +1,40 @@
+package youtube02;
+
+public class VeriTipleri {
+
+	public static void main(String[] args) {
+		int yas = 28;
+		char d = 'D';
+		String isim = "Deniz";
+		String soyisim = "İskender";
+		String phoneNumber = "+90***-***-***";
+		String meslek = "Bilgisayar mühendisliiği";
+		boolean yasiyormu = true;
+		
+		System.out.println("Yas: " + yas);
+		System.out.println("İsim: " + isim);
+		System.out.println("Soyad: " + soyisim);
+		System.out.println("Telefon: " + phoneNumber);
+		System.out.println("Meslek: " + meslek);
+		System.out.println("Yasiyor mu: " + yasiyormu);
+		
+		System.out.println();
+		
+		System.out.println("Youtube'da java anlatıyorum...");
+		System.out.println("Mobil oyunlar geliştiyorum...");
+		
+		yas = 45;
+		System.out.println("Yas: " + yas);
+		
+		System.out.println();
+		
+		System.out.println("Hayattan keyif alıyor...");
+		
+		yas = 80;
+		yasiyormu = false;
+		
+		System.out.println("Yas: " + yas);
+		System.out.println("Yasiyor mu: " + yasiyormu);
+		
+	}
+}
