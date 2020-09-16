3b784876260eaed281de7f9e790f59e82e9369ff
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-16 18:03:04 +0300

    Create Kosullar.java

---
+++ Java/06/Kosullar.java
@@ -0,0 +1,23 @@
+package youtube06;
+
+public class Kosullar {
+	public static void main(String[] args) {
+		int not = 91;
+		
+		if(not >= 90) {
+			System.out.println("A.");
+		} 
+		
+		else if(not >= 80) {
+			System.out.println("B.");
+		} 
+		
+		else if(not >= 70) {
+			System.out.println("C.");
+		} 
+		
+		else if(not >= 60) {
+			System.out.println("D.");
+		}
+	}
+}
