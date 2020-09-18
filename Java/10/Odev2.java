d2d5b9b9c72414554d6ebdfb4b1c1e44851a4bf0
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-18 21:50:37 +0300

    Create Odev2.java

---
+++ Java/10/Odev2.java
@@ -0,0 +1,12 @@
+package youtube10;
+
+public class Odev2 {
+
+	public static void main(String[] args) {
+		System.out.println("Max: " + maksimum(1.0, 2.0));
+	}
+
+	public static double maksimum(double sayi1, double sayi2) {
+		return Math.max(sayi1, sayi2);
+	}
+}
