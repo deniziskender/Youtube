127ee26b2a8621c00afcff823f8c6a580e9c4865
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-19 21:59:13 +0300

    Create LokalDeğişkenler.java

---
+++ Java/11/LokalDeğişkenler.java
@@ -0,0 +1,22 @@
+package youtube11;
+
+public class LokalDeğişkenler {
+	// method, if, for, {} blokları
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 5; i++) {
+			
+		}
+		
+		int i = 5;
+		
+		{
+			int j = 10;
+		}
+	}
+	
+	public static void m1() {
+		int k = 10;
+	}
+
+}
