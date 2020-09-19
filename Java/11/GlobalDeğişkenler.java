64d9c6095efebba67dc2f0e09bea88687391808e
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-19 21:59:00 +0300

    Create GlobalDeğişkenler.java

---
+++ Java/11/GlobalDeğişkenler.java
@@ -0,0 +1,22 @@
+package youtube11;
+
+public class GlobalDeğişkenler {
+	
+	static int y = 10;
+	
+	public static void main(String[] args) {
+		y = 10;
+		
+		int x = 6;
+		System.out.println("x: " + x);
+		
+		
+		m1();
+	}
+	
+	public static void m1() {
+		System.out.println("deshadjkhak");
+	}
+	
+	static int x = 5;
+}
