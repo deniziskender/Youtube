87e45e245c2a087aef42d43d19e9bcd8aec611c6
Deniz Iskender <deniz.iskender@ozu.edu.tr>
2020-09-21 18:40:59 +0300

    Create Ornek1.java

---
+++ Java/13/Ornek1.java
@@ -0,0 +1,27 @@
+package youtube13;
+
+public class Ornek1 {
+	public static void main(String[] args) {
+		int gun = 7;
+		switch (gun) {
+		case 1:
+			System.out.println("Pazartesi");
+			break;
+		case 2:
+			System.out.println("Salı");
+			break;
+		case 3:
+			System.out.println("Carsamba");
+			break;
+		case 4:
+			System.out.println("Persembe");
+			break;
+		case 5:
+			System.out.println("Cuma");
+			break;
+		default:
+			System.out.println("Hafta sonu");
+			break;
+		}
+	}
+}
